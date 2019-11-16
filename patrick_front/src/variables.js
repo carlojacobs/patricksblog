173d3ad3b295d20993dd851894a9cf6a9f039dc5
Carlo Jacobs <carlojacobs@Carlos-Air.domain_not_set.invalid>
2019-11-16 17:53:49 +0100

    Update

--- patrick_front/src/variables.js
+++ patrick_front/src/variables.js
@@ -1,2 +1,3 @@
-export const apiUrl = "https://patrickschrijft.nl/api";
+//export const apiUrl = "https://patrickschrijft.nl/api";
+export const apiUrl = "https://188.166.0.103/patrick";
 export const globalPassword = "Patrick4321!";
